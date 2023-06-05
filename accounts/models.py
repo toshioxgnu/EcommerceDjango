43bfe96566ab3179e6acfb5199013252cf04a294
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-06-05 19:01:21 -0400

    modified showing data in admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,36 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+
 class MyAccountManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError("User must have an email address")
         if not username:
             raise ValueError("User must have an username")
-        
+
         user = self.model(
-            email = self.normalize_email(email),
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, first_name, last_name, username, email, password):
         user = self.create_user(
-            email = self.normalize_email(email),
-            username = username,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
         )
         user.is_admin = True
         user.is_staff = True
         user.is_superuser = True
         user.is_active = True
-        
+
         user.save(using=self._db)
         return user
 
@@ -39,33 +40,37 @@ class MyAccountManager(BaseUserManager):
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, unique=True) # unique=True means that no two users can have the same username
+    # unique=True means that no two users can have the same username
+    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=50)
     password = models.CharField(max_length=500)
 
-    #reqired   
+    # reqired
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False) # is_admin is a boolean field that is set to False by default
-    is_staff = models.BooleanField(default=False) # is_staff is a boolean field that is set to False by default
-    is_superuser = models.BooleanField(default=False) # is_superuser is a boolean field that is set to False by default
-    is_active = models.BooleanField(default=False) # is_active is a boolean field that is set to False by default
+    # is_admin is a boolean field that is set to False by default
+    is_admin = models.BooleanField(default=False)
+    # is_staff is a boolean field that is set to False by default
+    is_staff = models.BooleanField(default=False)
+    # is_superuser is a boolean field that is set to False by default
+    is_superuser = models.BooleanField(default=False)
+    # is_active is a boolean field that is set to False by default
+    is_active = models.BooleanField(default=False)
 
-    #login field
-    USERNAME_FIELD = 'email' # USERNAME_FIELD is a required field and it must be unique
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name'] # REQUIRED_FIELDS is a list of fields that are required to create a user
+    # login field
+    USERNAME_FIELD = 'email'  # USERNAME_FIELD is a required field and it must be unique
+    # REQUIRED_FIELDS is a list of fields that are required to create a user
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     objects = MyAccountManager()
 
     def __str__(self):
         return self.email
 
-    #methods required by django
+    # methods required by django
     def has_perm(self, perm, obj=None):
         return self.is_admin
-    
+
     def has_module_perms(self, add_label):
         return True
-
-    
