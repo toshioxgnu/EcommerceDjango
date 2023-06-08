a5aae2295073f3f24ebf38a1f15be5bce25552a5
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-06-07 20:55:35 -0400

    Guardado

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,6 +24,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    
 ] + static(
     settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
 )
