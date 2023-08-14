71c089864050e07f701a55502320a4e62777c492
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-08-14 14:09:33 -0400

    paginator implemented

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from carts.models import CartItem
 from carts.views import _cart_id
 from .models import Product
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
@@ -15,26 +16,34 @@ def store(request, category_slug=None):
     if (category_slug != None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.all().filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
         products_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
         products_count = products.count()
 
     return render(request, 'store/store.html', context={
-        'products': products,
+        'products': page_products,
         'products_count': products_count,
     })
+
 
 def product_detail(request, category_slug, product_slug):
 
     try:
-        single_product = Product.objects.get( category__slug = category_slug, product_slug = product_slug)
-        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        single_product = Product.objects.get(
+            category__slug=category_slug, product_slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(
+            request), product=single_product).exists()
     except Exception as e:
         raise e
 
-    
     return render(request, 'store/product_detail.html', context={
-        'single_product' : single_product,
-        'in_cart' : in_cart,
+        'single_product': single_product,
+        'in_cart': in_cart,
     })
