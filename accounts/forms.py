1dec5358fc1fa7da9585b28a76ac18d1c0d254b3
Jose Gonzalez riquelme <jgonzalezr5@outlook.es>
2023-08-19 18:46:51 -0400

    password does not match error generatesd

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,23 +1,35 @@
-from django import forms 
+from django import forms
 from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder' : 'Enter Password',
+        'placeholder': 'Enter Password',
     }))
 
     confirmpassword = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder' : 'Confirm Password',
+        'placeholder': 'Confirm Password',
     }))
+
     class Meta:
         model = Account
-        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password','confirmpassword']
-    
+        fields = ['first_name', 'last_name', 'phone_number',
+                  'email', 'password']
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirmpassword')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        
+
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
         self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
