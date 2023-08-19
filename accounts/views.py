be15e1a2cd22bf6bf90b72af5b945cb7442c9d28
Jose Gonzalez riquelme <jgonzalezr5@outlook.es>
2023-08-19 03:23:49 -0400

    form control added by the class, template form passes by django now

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from accounts.forms import RegistrationForm
+
 # Create your views here.
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    form = RegistrationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/register.html', context)
 
 def login(request):
     return render(request, 'accounts/login.html')
