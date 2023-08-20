434768f27ffe64ae36efc3ce0b85d3d3d188a650
Jose Gonzalez riquelme <jgonzalezr5@outlook.es>
2023-08-20 14:24:47 -0400

    activation email configured

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,18 @@
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from accounts.forms import RegistrationForm
 from accounts.models import Account
 
-# Create your views here.
-
 
 def register(request):
-    if request.method == 'POST' :
+    if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             first_name = form.cleaned_data['first_name']
@@ -17,7 +20,7 @@ def register(request):
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            # take the first part of the email as username 
+            # take the first part of the email as username
             username = email.split('@')[0]
 
             user = Account.objects.create_user(
@@ -29,6 +32,20 @@ def register(request):
             )
             user.phone_number = phone_number
             user.save()
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/accountVerificactionEmail.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_b64encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user)
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
             messages.success(request, 'Registration Succesfull')
             return redirect('register')
     else:
@@ -56,7 +73,8 @@ def login(request):
 
     return render(request, 'accounts/login.html')
 
-@login_required( login_url = 'login' )
+
+@login_required(login_url='login')
 def logout(request):
     auth.logout(request)
     messages.success(request, 'You are Logout')
