c80ea6122559ba70f7e3ef5594c82e83f28fecd7
Jose Gonzalez riquelme <jgonzalezr5@outlook.es>
2023-09-04 21:13:15 -0300

    reset password email generated

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,13 @@ from . import views
 urlpatterns = [
     path('register/',views.register, name='register'),
     path('login/',views.login, name='login'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('logout/',views.logout, name='logout'),
     path('dashboard/',views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
 
-    path('activate/<uidb64>/<token>', views.activate, name='activate')
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('resetPasswordValidate/<uidb64>/<token>', views.activate, name='resetPasswordValidate'),
+
 ]
